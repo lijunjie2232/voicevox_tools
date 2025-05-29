c5cb8e181b75d483a8272a94cd75ed68d1167b01
lijunjie <2533584225@qq.com>
2025-05-29 22:50:27 +0800

    voicevox

--- main.py
+++ main.py
@@ -51,6 +51,11 @@ def get_args():
         help="speaker uuid",
     )
     parser.add_argument(
+        "--exact_name",
+        action="store_true",
+        help="query speaker name by exact match",
+    )
+    parser.add_argument(
         "--query_only",
         action="store_true",
         help="query speaker info only",
@@ -66,52 +71,70 @@ def get_args():
         hook_params[key] = value
     return args, hook_params
 
+
+def print_speaker_styles(speaker_styles):
+    for sss in speaker_styles:
+
+        print(f"********************** {sss.name} **********************")
+        print(f"speaker_uuid: {sss.speaker_uuid}")
+        print("----------------------- styles -----------------------")
+        pprint(sss.styles)
+        print("----------------- supported features -----------------")
+        pprint(sss.supported_features)
+        print("------------------------------------------------------")
+        print(f"version: {sss.version}")
+        print()
+
+
 def main(args, hook_params):
+    # init voicevox engine
     engine = VoicevoxEngine(base_url=args.base_url)
+    # get args
     out_dir = Path(args.out_dir)
     txt_file = Path(args.txt)
     speaker_uuid = args.speaker_uuid
     speaker_name = args.speaker_name
     speaker_id = args.speaker_id
+    
+    # init speaker id
     if speaker_id < 0:  # get speaker_id first
         if not speaker_uuid and not speaker_name:
             if args.query_only:
                 speakers = engine.speakers
-                pprint(speakers)
+                # pprint(speakers)
+                print_speaker_styles(speakers)
                 return
             else:
                 raise ValueError(
                     "speaker_uuid or speaker_name must be provided or specify query_only."
                 )
         else:
-            speaker_styles = speakers.get_speaker_style(
+            print("----------- match by -----------")
+            print(f"speaker uuid: {speaker_uuid}")
+            print(f"speaker name: {speaker_name}")
+            print(f"exactly match: {args.exact_name}")
+            print("--------------------------------")
+            speaker_styles = engine.get_speaker_style(
                 speaker_uuid=speaker_uuid,
-                speaker_name=speaker_name,
+                name=speaker_name,
+                amb_match=not args.exact_name,
             )
             if len(speaker_styles) == 0:
-                ValueError("speaker_uuid or speaker_name is not found.")
+                print("no matched speaker found.")
                 return
+            print_speaker_styles(speaker_styles)
             if args.query_only:
-                pprint(speaker_styles)
                 return
-            if len(speaker_styles) > 1:
-                # pprint(speaker_styles)
-                # pprint("speaker_uuid or speaker_name is ambiguous.")
-                # speaker_id = int(input("Press specify speaker id to continue: "))
-                for ss in speaker_styles:
-                    print("**********************************")
-                    print(f"name: {ss.name}")
-                    print(f"speaker_uuid: {ss.speaker_uuid}")
-                    print("------------- styles -------------")
-                    pprint(ss.styles)
-                    print("------- supported features -------")
-                    pprint(ss.supported_features)
-                    print("----------------------------------")
-                    print(f"version: {ss.version}")
-                    print("**********************************")
-            else:
-                speaker_id = speaker_styles[0]
-
+            speaker_id = int(input("Press specify speaker id to continue: "))
+    try:
+        # engine.speaker_init(
+        #     speaker_id=speaker_id,
+        # )
+        print(f"speaker_id: {speaker_id}")
+    except Exception:
+        pass
+    
+        
 
 if __name__ == "__main__":
     args, hook_params = get_args()
