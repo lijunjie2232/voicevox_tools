56631712471a17c9bd0bc0f417bacc169b2142fa
lijunjie2232 <2533584225@qq.com>
2025-08-03 11:12:51 +0900

    wav data

--- Compressor.py
+++ Compressor.py
@@ -7,13 +7,28 @@ class Compressor:
         self.out_fmt = out_fmt
         self.bitrate = bitrate
 
-    def compress(self, in_file, out, overwrite=False):
-        if Path(out).is_dir():
-            out = out / f"{in_file.stem}.{self.out_fmt}"
-        if Path(out).is_file() and not overwrite:
+    def compress(
+        self,
+        data=None,
+        in_file=None,
+        out_file=None,
+        overwrite=False,
+    ):
+        sound = None
+        assert out_file, "out_file is not specified"
+        out_file = Path(out_file)
+        assert out_file.parent.exists(), f"{out_file.parent} is not found"
+        if isinstance(data, bytes):
+            sound = AudioSegment(data)
+        elif isinstance(in_file, Path) or isinstance(in_file, str):
+            sound = AudioSegment.from_wav(in_file)
+        else:
+            raise ValueError("data or in_file is not valid")
+        if out_file.is_dir() and in_file is not None:
+            out_file = out_file / f"{in_file.stem}.{self.out_fmt}"
+        if out_file.is_file() and not overwrite:
             return
-        sound = AudioSegment.from_wav(in_file)
-        sound.export(out, format=self.out_fmt, bitrate=self.bitrate)
+        sound.export(out_file, format=self.out_fmt, bitrate=self.bitrate)
 
 
 if __name__ == "__main__":
